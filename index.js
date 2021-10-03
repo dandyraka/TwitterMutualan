2773c1f2ff56b13f55529e080817b686ca24f5d7
dandyraka <dandyraka13@gmail.com>
2021-10-04 02:02:16 +0700

    NEW

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
 const Twitter = require('twitter-lite');
 const cron = require('node-cron');
 const color = require('./utils/color');
 const db = require('./db');
-require('dotenv').config()
+const delay = require('delay');
+const cliSpinners = require('cli-spinners');
+const ora = require('ora');
 
 //Banner
 console.log(color(`%c _______       _ _   _            __  __       _               _             
@@ -22,6 +25,13 @@ const autoFollow = process.env.auto_follow.toUpperCase();
 const cron_findMutual = process.env.cron_findMutual;
 const cron_autoFollow = process.env.cron_autoFollow;
 const cron_reset = process.env.cron_reset;
+const base_list = process.env.base_target;
+const array_base = base_list.split('|');
+
+const spinner = ora({
+	text: 'Delay...',
+	spinner: cliSpinners.moon,
+});
 
 const client = new Twitter({
     subdomain: "api",
@@ -32,131 +42,142 @@ const client = new Twitter({
     access_token_secret: process.env.access_token_secret
 });
 
-console.log(color('\n\nStarting...', 'green'));
+console.log(color('\n\nStarting...', 'cyan'));
+console.log(`${color(`Total base: ${array_base.length}`, 'cyan')}\n`);
 
-client.get("account/verify_credentials")
-    .catch((e) => {
-        if (e.errors[0].code) console.log(color('[ERROR]', 'red'), e.errors[0].message) 
-        process.exit(1)
-    })
+client.get("account/verify_credentials").catch((e) => {
+    if (e.errors[0].code) console.log(color('[ERROR]', 'red'), e.errors[0].message) 
+    process.exit(1)
+})
 
 const isMutual = (tweet) => {
-    return tweet.match(new RegExp(/mutu((a|4)*)l((a|4)*n)?/g));
+    return tweet.match(new RegExp(/mutu((a|4)*)l((a|4)*n)?/gi));
 }
 
 const isIgnored = (tweet) => {
-    return tweet.match(new RegExp(/kpop|korea|stan|ig|drop|link|ig|instagram|wa|whatsapp|watsap|army|(\-)?m\-?(\d+)|pic\.twitter\.com/g));
+    return tweet.match(new RegExp(/kpop|korea|stan|ig|drop|link|ig|instagram|wa|whatsapp|watsap|army|(\-)?m\-?(\d+)|pic\.twitter\.com/gi));
 }
 
+function spiner(){
+    setTimeout(() => {
+        spinner.start();
+    }, 1000);
+}
 async function getTweets(userlist) {
     for (const user of userlist) {
         try {
-            const tweets = await client.get("statuses/user_timeline", { screen_name: user, count: 1 }).catch(error => error);
-            const tweetID = tweets[0].id_str
-            const tweetText = tweets[0].text
-            const isSaved = await db.findTweet(tweetID)
+            const tweets = await client.get("statuses/user_timeline", { screen_name: user, count: 1 }).catch(() => {
+                return `${color('[ERROR]', 'red')} ${color(user, 'yellow')} not found!!`;
+            });
+            const tweetID = tweets[0].id_str;
+            const tweetText = tweets[0].text;
+            const isSaved = await db.findTweet(tweetID);
 
-            if (isSaved) console.log(color('[ALREADY_RETWEETED]', 'red'), '=>', color(tweetID))
+            (isSaved) ? console.log(color('[ALREADY_RETWEETED]', 'red'), '=>', color(tweetID)) : "";
             if (isMutual(tweetText)) {
-                await db.addTweet(tweetID)
-                console.log(color('[MUTUAL_FOUND]', 'green'), 'on', color(user, 'yellow'))
+                await db.addTweet(tweetID);
+                console.log(color('[MUTUAL_FOUND]', 'green'), 'on', color(user, 'yellow'));
 
-                if (isIgnored(tweetText)) return console.log(color('[IGNORED]', 'red'), 'Mengandung kata cringe')
+                if (isIgnored(tweetText)) return console.log(color('[IGNORED]', 'red'), 'Mengandung kata cringe');
 
-                const doRetweet = await client.post(`statuses/retweet/${tweetID}`).catch(error => error);
-                if (doRetweet.retweeted) console.log(color('[RETWEETED]', 'green'), '=>', color(tweetID))
+                const doRetweet = await client.post(`statuses/retweet/${tweetID}`).catch(() => {
+                    return `${color('[ERROR]', 'red')} failed to retweet ${color(tweetID, 'yellow')}`;
+                });
+                (doRetweet.retweeted) ? console.log(color('[RETWEETED]', 'green'), '=>', color(tweetID)) : "";
             } else {
-                console.log(color('[MUTUAL_NOTFOUND]', 'red'), 'on', color(user, 'yellow'))
+                console.log(color('[MUTUAL_NOTFOUND]', 'red'), 'on', color(user, 'yellow'));
             }
-
+            await delay(2000);
         } catch (e) {
             if ('errors' in e) {
                 // Twitter API error
-                if (e.errors[0].code === 88) return console.log(color('[ERROR]', 'red'), "Rate limit will reset on", new Date(e._headers.get("x-rate-limit-reset") * 1000))
-                if (e.errors[0].code) return console.log(color('[ERROR]', 'red'), e.errors[0].message)
+                if (e.errors[0].code === 88) return console.log(color('[ERROR]', 'red'), "Rate limit will reset on", new Date(e._headers.get("x-rate-limit-reset") * 1000));
+                if (e.errors[0].code) return console.log(color('[ERROR]', 'red'), e.errors[0].message);
             } else {
                 // non-API error, e.g. network problem or invalid JSON in response
-                console.log(color('[ERROR]', 'red'), e)
+                console.log(color('[ERROR]', 'red'), e);
             }
         }
     }
+    //console.log(color('Delay...', 'cyan'));
+    spiner();
 }
 
 async function retweeters() {
     try {
-        const tweet = await db.getAllTweet()
-        tweet.forEach(async (tweets) => {
-            const tweetID = tweets.id
-            const get_retweeters = await client.get(`statuses/retweets/${tweetID}`).catch(error => error);
+        const tweet = await db.getAllTweet();
+        await tweet.forEach(async (tweets) => {
+            const tweetID = tweets.id;
+            const get_retweeters = await client.get(`statuses/retweets/${tweetID}`).catch(() => {
+                return `${color('[ERROR]', 'red')} failed to get retweeters`;
+            });
             get_retweeters.forEach(async function (retweetersID) {
-                const retweeterID = retweetersID.user.id_str
-                const isSaved = await db.findUser(retweeterID)
-                if (!isSaved) await db.addUser(retweeterID, 'belum')
+                const retweeterID = retweetersID.user.id_str;
+                const isSaved = await db.findUser(retweeterID);
+                if (!isSaved) await db.addUser(retweeterID, 'belum');
             })
+            await delay(2000);
         });
     } catch (e) {
-        console.log(color('[ERROR]', 'red'), e)
+        console.log(color('[ERROR]', 'red'), e);
     }
 
 }
 
 async function follow() {
     try {
-        const usersToFollow = await db.filterUser('belum')
-        usersToFollow.forEach(async (user) => {
-            const userCheck = await client.post("users/lookup", { user_id: user.id }).catch(error => error);
+        const usersToFollow = await db.filterUser('belum');
+        await usersToFollow.forEach(async (user) => {
+            const userCheck = await client.post("users/lookup", { user_id: user.id }).catch(() => {
+                return `${color('[ERROR]', 'red')} failed to lookup ${color(userCheck.screen_name, 'yellow')}`;
+            });
             if (userCheck.errors) {
-                await db.updateUserStatus(user.id, 'error')
+                await db.updateUserStatus(user.id, 'error');
             } else {
                 const doFollow = await client.post("friendships/create", { user_id: user.id }).catch(error => error);
                 if (!doFollow.errors) {
-                    await db.updateUserStatus(user.id, 'success')
-                    console.log(color('[FOLLOWED]', 'green'), '=>', color(doFollow.screen_name))
+                    await db.updateUserStatus(user.id, 'success');
+                    console.log(color('[FOLLOWED]', 'green'), '=>', color(doFollow.screen_name));
                 }
             }
+            await delay(2000);
         });
+        spiner();
     } catch (e) {
-        console.log(color('[ERROR]', 'red'), e)
+        console.log(color('[ERROR]', 'red'), e);
     }
 
 }
 
-const listUser = [
-    "MUTUALANDFESS",
-    "MUTUALANDFESS1",
-    'menfesssyg', 
-    'subtanyarl2', 
-    'sygfess',
-    'spongebobmnfess',
-    'bacotfess',
-    'sqwfess',
-    'mutualan_base'
-];
-getTweets(listUser)
-autoFollow === "ON" ? retweeters() : ''
-autoFollow === "ON" ? follow() : ''
+getTweets(array_base);
+autoFollow === "ON" ? retweeters() : '';
+autoFollow === "ON" ? follow() : '';
 
 cron.schedule(`*/${cron_findMutual} * * * *`, () => {
-    console.log(color('=== FIND MUTUAL IN BASE ===', 'green'))
-    getTweets(listUser)
+    spinner.succeed();
+    console.log(color('=== FIND MUTUAL IN BASE ===', 'pink'));
+    getTweets(array_base);
 });
 
 if (autoFollow === "ON") {
     cron.schedule(`*/${cron_autoFollow} * * * *`, () => {
-        console.log(color('=== AUTO FOLLOW RETWEETERS ===', 'green'))
-        follow()
+        spinner.succeed();
+        console.log(color('=== AUTO FOLLOW RETWEETERS ===', 'pink'));
+        follow();
     });
 }
 
 cron.schedule(`*/${cron_reset} * * * *`, async () => {
-    console.log(color('=== RESET DATABASE ===', 'green'))
+    spinner.succeed();
+    console.log(color('=== RESET DATABASE ===', 'pink'));
     //Unretweet
-    const retweetList = await db.getAllTweet()
+    const retweetList = await db.getAllTweet();
     retweetList.forEach(async (retweets) => await client.post(`statuses/unretweet/${retweets.id}`).catch(error => error));
     
     await db.clearAllTweet()
     if (autoFollow === "ON") {
-        await db.removeUserByStatus("error")
-        retweeters()
+        await db.removeUserByStatus("error");
+        retweeters();
     }
+    console.log(color('Berhasil reset database & unretweet!', 'pink'));
 });
